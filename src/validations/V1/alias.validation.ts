6a031dd645be968faee9ed5bd66388df854d02f4
Bramanda Suwandi <bramanda48@gmail.com>
2024-03-07 19:27:00 +0700

    feat: change snake_case to camelCase

--- src/validations/V1/alias.validation.ts
+++ src/validations/V1/alias.validation.ts
@@ -3,24 +3,15 @@ import { z } from "../../../deps.ts";
 export const AliasValidation = {
   createAlias: z
     .object({
-      email_alias: z.string().email(),
-      email_recipient: z.array(z.string().email()),
+      emailAlias: z.string().email(),
+      emailRecipient: z.array(z.string().email()),
     })
-    .refine((param) => !param.email_recipient.includes(param.email_alias), {
-      message: "Must not contain email_alias",
-      path: ["email_recipient"],
-    })
-    .transform((param) => ({
-      emailAlias: param.email_alias,
-      emailRecipient: param.email_recipient,
-    })),
-  removeAlias: z
-    .object({
-      email_alias: z.string().email(),
-      email_recipient: z.string().email(),
-    })
-    .transform((param) => ({
-      emailAlias: param.email_alias,
-      emailRecipient: param.email_recipient,
-    })),
+    .refine((param) => !param.emailRecipient.includes(param.emailAlias), {
+      message: "Must not contain emailAlias",
+      path: ["emailRecipient"],
+    }),
+  removeAlias: z.object({
+    emailAlias: z.string().email(),
+    emailRecipient: z.string().email(),
+  }),
 };
