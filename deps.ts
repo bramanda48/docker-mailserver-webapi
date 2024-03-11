6cdee58ff359af04a92587eec00443ab9fbe0d2e
Bramanda Suwandi <bramanda48@gmail.com>
2024-03-11 17:50:57 +0700

    feat: load environment variable file (.env)

--- deps.ts
+++ deps.ts
@@ -2,6 +2,7 @@ export * as path from "std/path/mod.ts";
 export * as fs from "std/fs/mod.ts";
 export * as bytes from "std/bytes/mod.ts";
 export * as assert from "std/assert/mod.ts";
+export * as dotenv from "std/dotenv/mod.ts";
 export * from "hono";
 export * from "hono/cors";
 export * from "hono/pretty-json";
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { swaggerUI } from "hono/swagger-ui";
-import { Hono, cors, fs, prettyJSON, serveStatic } from "../deps.ts";
+import { Hono, cors, dotenv, fs, prettyJSON, serveStatic } from "../deps.ts";
 import { notFoundHandler } from "./middlewares/error.middleware.ts";
 import { errorHandler } from "./middlewares/error.middleware.ts";
 import { DefaultRoute } from "./routes/base.route.ts";
 
 const app = new Hono();
 const cwd = Deno.cwd();
+
+// environment variables
+dotenv.loadSync({
+  allowEmptyValues: true,
+  export: true,
+});
 
 // middleware
 app.use("*", cors());
