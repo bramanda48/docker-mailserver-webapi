3abd006fec0920e166981f7b4770d0726785c667
Bramanda Suwandi <bramanda48@gmail.com>
2024-03-16 20:53:30 +0700

    feat: add FQDN custom validation

--- src/validations/V1/dkim/dkim.validation.ts
+++ src/validations/V1/dkim/dkim.validation.ts
@@ -1,9 +1,10 @@
 import { z } from "../../../../deps.ts";
+import { utils } from "../../../utils/utils.ts";
 
 export const DkimValidation = {
   createKey: z.object({
     selector: z.string(),
-    domain: z.string(),
+    domain: z.custom(utils.isFQDN, { message: "Invalid. Must be FQDN" }),
     keytype: z.string(),
     keysize: z.number(),
   }),
--- src/validations/V1/relay/relay.validation.ts
+++ src/validations/V1/relay/relay.validation.ts
@@ -1,17 +1,18 @@
 import { z } from "../../../../deps.ts";
+import { utils } from "../../../utils/utils.ts";
 
 export const RelayValidation = {
   addAuth: z.object({
-    domain: z.string(),
+    domain: z.custom(utils.isFQDN, { message: "Invalid. Must be FQDN" }),
     username: z.string(),
     password: z.string(),
   }),
   addDomain: z.object({
-    domain: z.string(),
+    domain: z.custom(utils.isFQDN, { message: "Invalid. Must be FQDN" }),
     host: z.string(),
     port: z.number(),
   }),
   excludeDomain: z.object({
-    domain: z.string(),
+    domain: z.custom(utils.isFQDN, { message: "Invalid. Must be FQDN" }),
   }),
 };
